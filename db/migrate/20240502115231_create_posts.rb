f3b13a1147ba7929fcf4e58837341543af5bfebf
shimaa <shimaafathi126@gmail.com>
2024-05-02 14:54:32 +0300

    generate-scaffold

--- db/migrate/20240502115231_create_posts.rb
+++ db/migrate/20240502115231_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :content
+      t.text :content
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_073411) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_115231) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
 end
